88555e3a2a302afe535b13a98245f29ec2ebf178
slystersanto <113336021+slystersanto@users.noreply.github.com>
2022-11-25 13:17:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-// let obj={  =>for loop
-//     "name":"slyster",
-//     "age":"95",
-//     "gender":"male",
-//     "degree":["Bsc","Msc","M.phil",]
-// }
-//  for(var i=0; i<obj.degree.length;i++){
-//     console.log(obj.degree[i]);
-//  }
+ let obj={  =>for loop
+     "name":"slyster",
+    "age":"95",
+    "gender":"male",
+     "degree":["Bsc","Msc","M.phil",]
+ }
+  for(var i=0; i<obj.degree.length;i++){
+     console.log(obj.degree[i]);
+  }
 
 
 
-// let obj={    =>for in loop
-//     "name":"slyster",
-//     "age":"95",
-//     "gender":"male",
-//     "degree":["Bsc","Msc","M.phil",]
-// }
-//  for(var i in obj.degree){
-//     console.log(obj.degree[i]);
-//  }
+ let obj={    =>for in loop
+    "name":"slyster",
+     "age":"95",
+     "gender":"male",
+     "degree":["Bsc","Msc","M.phil",]
+}
+  for(var i in obj.degree){
+     console.log(obj.degree[i]);
+  }
 
 
-// let Obj = {  =>for of loop
-//     "name": "slyster",
-//     "age": "95",
-//     "gender": "Male",
-//     "degree":["Bsc","Msc","M.phil"]
-// }
+let Obj = {  =>for of loop
+     "name": "slyster",
+     "age": "95",
+     "gender": "Male",
+     "degree":["Bsc","Msc","M.phil"]
+ }
 
-// for (let i of Object.keys(Obj)) {
-//     console.log( i, Obj[i]);
-// }
+ for (let i of Object.keys(Obj)) {
+     console.log( i, Obj[i]);
+ }
 
